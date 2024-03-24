c97f1f77ecdf9535d4227820cd1d977442904035
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2024-03-24 17:26:51 +0700

    Update register.php

--- register.php
+++ register.php
@@ -8,7 +8,7 @@ include "db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrasi</title>
+    <title>Register</title>
 </head>
 
 <body>
